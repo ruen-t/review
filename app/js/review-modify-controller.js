443b578ffa7946130437f069bf8d06c73c515212
System Administrator <root@Angelas-MacBook-Air.local>
2017-02-14 19:11:45 +0900

    modify first addreview

--- app/js/review-modify-controller.js
+++ app/js/review-modify-controller.js
@@ -1,12 +1,39 @@
+var user = {
+  title: 'Developer',
+  email: 'ipsum@lorem.com',
+  firstName: '',
+  lastName: '',
+  company: 'Google',
+  address: '1600 Amphitheatre Pkwy',
+  city: 'Mountain View',
+  state: 'CA',
+  biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+  postalCode: '94043'
+    };
+var states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' + 'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' + 'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+      });
+
+
 
 var ReviewModifyController =['$resource','$translate', function ($resource,$translate) {
     var vm = this;
-    vm.message = "hello this is a add page"
-   
-   
-}]
-angular.module('reviewmodify', ['datatables', 'ngResource'])
-.controller('ReviewModifyController', ReviewModifyController);
+    vm.message = "hello this is a add page";
+    vm.user = user;
+    vm.states=states;
+  }]
+
+ angular.module('reviewmodify', ['datatables', 'ngResource'])
+.controller('ReviewModifyController', ReviewModifyController)
+.config(function($mdThemingProvider) {
+
+    // Configure a dark theme with primary foreground yellow
+
+    $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();
+
+  });
 
 
 
